9545e34e706b8bd6ba2a3c981dfc1b272d938372
ferym26 <ferym26@gmail.com>
2020-08-06 15:49:10 +0300

    scripts concat added

--- gulp-tasks/scripts-libs.js
+++ gulp-tasks/scripts-libs.js
@@ -2,17 +2,20 @@ module.exports = function(gulp, options, plugins) {
 
 	return function (cd) {
 		gulp.src([
+			'./src/assets/vendors/jquery/jquery-3.4.1.min.js',
 			'./src/assets/vendors/**/*.js',
 			'!./src/assets/js/main.js',
 			'!./src/assets/js/init.js',
 		], {
-			base: 'src/assets/vendors/'
+			base: 'src/assets/'
 		})
 			.pipe(plugins.uglify())
+			.pipe(plugins.concat('libs.js'))
 			.pipe(plugins.rename({
+				suffix: '.min',
 				dirname: '', //убирает путь из имени файла и в dest идут только файлы без вложения в папки
 			}))
-			.pipe(gulp.dest('./app/js/vendors/'));
+			.pipe(gulp.dest('./app/js/'));
 
 		cd();
 	}
